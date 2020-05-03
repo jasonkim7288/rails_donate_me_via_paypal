8b8a7aaa2fdf39975a8ec625e7394fb2ceb9bce8
jasonkim7288 <jason.kim7288@gmail.com>
2020-05-03 21:03:27 +1000

    add columns to User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/index'
   get 'pages/donation'
   get 'pages/management'
+  root 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
